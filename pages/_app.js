165d65f449aa6baaac472df426367778465001d3
Shiv Singh <shivsingh7150@hotmail.com>
2024-01-04 17:06:49 +0530

    app updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,17 @@
+import "/styles/style.css";
+import "/styles/plugins.css";
+import "/styles/fontawesome.css";
+import "/styles/theme-change.css";
+import "/styles/reset.css";
 
-import '/styles/style.css'
-import '/styles/plugins.css'
-import '/styles/fontawesome.css'
-import '/styles/theme-change.css'
-import '/styles/reset.css'
-
-
-import {ThemeProvider as NextThemesProvider} from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-
-      <NextThemesProvider>
-        <Component {...pageProps} />
-      </NextThemesProvider>
-
-  )
+    <NextThemesProvider>
+      <Component {...pageProps} />
+    </NextThemesProvider>
+  );
 }
 
 export default MyApp;
